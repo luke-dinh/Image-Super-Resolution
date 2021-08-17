41e40a7aff40da9524b345d4011860d9930bf8f4
luke-dinh <dinhphuoclockt@gmail.com>
2021-08-17 22:00:27 +0700

    changes

--- mSRGAN/test_image.py
+++ mSRGAN/test_image.py
@@ -7,12 +7,12 @@ from torch.autograd import Variable
 from torchvision.transforms import ToTensor, ToPILImage
 
 from model import Generator
-
+# torch.cuda.empty_cache()
 parser = argparse.ArgumentParser(description='Test Single Image')
 parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
 parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
 parser.add_argument('--image_name', type=str, help='test low resolution image name')
-parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
+parser.add_argument('--model_name', default='netG_epoch_4_49.pth', type=str, help='generator model epoch name')
 opt = parser.parse_args()
 
 UPSCALE_FACTOR = opt.upscale_factor
@@ -21,20 +21,30 @@ IMAGE_NAME = opt.image_name
 MODEL_NAME = opt.model_name
 
 model = Generator(UPSCALE_FACTOR).eval()
+
+# For images with jpg or jpeg format:
 if TEST_MODE:
     model.cuda()
-    model.load_state_dict(torch.load('epochs_msrgan/' + MODEL_NAME))
+    model.load_state_dict(torch.load('epochs_msrgan_celeba/' + MODEL_NAME))
 else:
-    model.load_state_dict(torch.load('epochs_msrgan/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+    model.load_state_dict(torch.load('epochs_msrgan_celeba/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+
+# # For images with png format:
+# if TEST_MODE:
+#     model.cuda()
+#     model.load_state_dict(torch.load('epochs_msrgan/' + MODEL_NAME))
+# else:
+#     model.load_state_dict(torch.load('epochs_msrgan/' + MODEL_NAME, map_location=lambda storage, loc: storage))
 
 image = Image.open(IMAGE_NAME)
-image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
+with torch.no_grad():
+  image = Variable(ToTensor()(image)).unsqueeze(0)
 if TEST_MODE:
     image = image.cuda()
 
-start = time.clock()
+start = time.process_time()
 out = model(image)
-elapsed = (time.clock() - start)
+elapsed = (time.process_time() - start)
 print('cost' + str(elapsed) + 's')
 out_img = ToPILImage()(out[0].data.cpu())
 out_img.save('out_srf_' + str(UPSCALE_FACTOR) + '_' + IMAGE_NAME)
