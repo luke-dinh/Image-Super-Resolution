99a1bf602036f43f2a64eb78841421f9855e5abd
luke-dinh <dinhphuoclockt@gmail.com>
2021-08-23 10:19:36 +0700

    changes

--- app.py
+++ app.py
@@ -25,12 +25,14 @@ parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU
 parser.add_argument('--image_name', type=str, help='test low resolution image name')
 parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
 parser.add_argument('--directory', default='mSRGAN/epochs_msrgan/', type=str, help='directory to the model')
+parser.add_argument('--model_name_celeba', default='netG_epoch_4_80.pth', type=str, help='generator model for jpg format')
 opt = parser.parse_args()
 
 UPSCALE_FACTOR = opt.upscale_factor
 TEST_MODE = True if opt.test_mode == 'GPU' else False
 IMAGE_NAME = opt.image_name
 MODEL_NAME = opt.model_name
+MODEL_NAME_CELEBA = opt.model_name_celeba
 DIR = opt.directory
 
 model = model.Generator(UPSCALE_FACTOR).eval()
@@ -42,13 +44,13 @@ model = model.Generator(UPSCALE_FACTOR).eval()
 #     else:
 #         model.load_state_dict(torch.load(model_dir + MODEL_NAME, map_location=lambda storage, loc:storage))
 
-def generate_img(model, model_dir, image_name):
+def generate_img(model, model_dir, model_name, image_name):
 
     if TEST_MODE:
         model.cuda()
-        model.load_state_dict(torch.load(model_dir + MODEL_NAME))
+        model.load_state_dict(torch.load(model_dir + model_name))
     else:
-        model.load_state_dict(torch.load(model_dir + MODEL_NAME, map_location=lambda storage, loc:storage))
+        model.load_state_dict(torch.load(model_dir + model_name, map_location=lambda storage, loc:storage))
 
     image = Image.open(image_name)
     with torch.no_grad():
@@ -79,10 +81,10 @@ def upload():
 
         if str(file_path).endswith('.png'):
             model_dir = DIR
-            result = generate_img(model = model, model_dir=model_dir, image_name=file_path)
+            result = generate_img(model = model, model_dir=model_dir, model_name= MODEL_NAME, image_name=file_path)
         else:
             new_model_dir = 'mSRGAN/epochs_msrgan_celeba/'
-            result = generate_img(model=model, model_dir=new_model_dir, image_name=file_path)
+            result = generate_img(model=model, model_dir=new_model_dir, model_name=MODEL_NAME_CELEBA , image_name=file_path)
 
         return result
 
